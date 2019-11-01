237891cdebe6425afb0835537a92827e40dc3c2a
phq <1153606328@qq.com>
2019-11-01 22:06:07 +0800

    引入mintUi，自定义组件nav

---
+++ src/components/Navigation.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="navigation">
+    <div class="box" :style="boxWidth">
+      <div class="left">
+        <i :class="iconShow==true?'iconfont iconshang':'iconfont iconxia'" @click="popUp" ></i>
+        <p class="ml10">oneMan</p>
+      </div>
+      <div class="right" v-show="iconShow">
+        <i class="iconfont iconsousuo"></i>
+        <i class="iconfont icondiandian"></i>
+      </div>
+    </div>
+    <!-- <mt-actionsheet
+  :actions="arr"
+  v-model="iconShow">
+</mt-actionsheet> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Navigation',
+  data () {
+    return {
+      iconShow: true,
+      boxWidth:{width:"100%"},
+      arr:[
+          {name:"1",method:()=>console.log("1")},
+          {name:"2",method:()=>console.log("2")}
+      ]
+    }
+  },
+  methods:{
+    popUp(){
+      if(this.iconShow==true){
+        this.iconShow=false;
+        this.boxWidth={width:"50%"}
+      }else{
+        this.iconShow=true;
+        this.boxWidth={width:"100%"}
+      }
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.navigation{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  .box{
+    background: #ffcdd2;
+    height: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    transition: width 1s;
+    .left{
+      display: flex;
+      align-items: center;
+      padding-left: 10px;
+    }
+    .right{
+      margin-right: 10px;
+    }
+  }
+  .ml10{margin-left: 10px;}
+}
+</style>
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="index">
+    <mt-header  title="OneMan"></mt-header>
+    <p class="title">My2019</p>
+    <div class=""></div>
+    <v-navigation></v-navigation>
+  </div>
+</template>
+
+<script>
+import navigation from "../components/Navigation.vue"
+export default {
+  name: 'HelloWorld',
+  components: {//注册组件
+    'v-navigation': navigation
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.index{
+    .title{
+      padding: 10px;
+    }
+}
+</style>
